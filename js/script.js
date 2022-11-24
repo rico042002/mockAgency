1e2bcbb1826180e9586fa9ec247d6502d89ec3d0
Rico042002 <rico042002@gmail.com>
2022-11-24 04:00:38 -0600

    commit 4

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ const navigation = document.querySelector(".navigation");
 
 toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
 });
